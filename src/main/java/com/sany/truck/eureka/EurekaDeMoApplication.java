2a6a18a9ed680343a6a884d4680dadef59f67d9f
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2020-12-24 16:24:50 +0800

    first commit

---
+++ src/main/java/com/sany/truck/eureka/EurekaDeMoApplication.java
@@ -0,0 +1,14 @@
+package com.sany.truck.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaDeMoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDeMoApplication.class, args);
+    }
+}
